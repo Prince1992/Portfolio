fa8869bd31fd9734b8554e5893abb238cf86da45
Amar Singh Yadav <amarprince9@gmail.comm>
2022-07-20 14:14:09 +0530

    Worked on topbar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Contact from './components/contact/Contact';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/protfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Topbar from './components/topbar/Topbar';
+import Works from './components/works/Works';
+import './app.scss';
+import { useState } from "react";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Topbar>
+      <div className="sections">
+        <Intro></Intro>
+        <Portfolio></Portfolio>
+        <Works></Works>
+        <Testimonials></Testimonials>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import './topbar.scss'
+import { Email, Person } from '@material-ui/icons';
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar " +(menuOpen && "active")}>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>genius.</a>
+          <div className="itemContainer">
+            <Person className='icon'></Person>
+            <span>+91 7704824710</span>
+          </div> 
+          <div className="itemContainer">
+            <Email className='icon'></Email>
+            <span>amarprince9@gmail.com</span>
+          </div> 
+        </div>
+        
+        <div className='right'>
+        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <span className='line1'></span>
+          <span className='line2'></span>
+          <span className='line3'></span>
+        </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
